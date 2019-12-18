45319cad5e7bdaccf49f337ddf451069bd09fc8d
Sergey Myakishev <sergey.myakishev@columbusglobal.com>
2019-12-19 00:31:21 +0300

    add cors, add text/plain support

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const MongoClient    = require('mongodb').MongoClient;
 const assert         = require('assert');
+const cors           = require('cors');
 const app            = require('express')();
 const dbConn = "mongodb://localhost:27017";
 const dbName = "vending";
 const collectionName = "logs";
 
 app.use(require('body-parser').json());
+app.use(require('body-parser').text());
+app.use(cors());
+
+
+app.options('*', cors());
 
 app.post('/', (req, res) => {
     console.log(req.body);
@@ -14,7 +20,11 @@ app.post('/', (req, res) => {
         assert.equal(null, err);
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
-        collection.insertOne(req.body);
+        let doc = req.body;
+        if (typeof req.body === 'string') {
+            doc = JSON.parse(doc);
+        }
+        collection.insertOne(doc);
         client.close();
     });
 
